541f381690351575615819f36d0f857725395b5e
hdeiner <howard.deiner@deinersoft.com>
2018-01-22 16:13:08 -0500

    Initial commit.

---
+++ SpecFlowSelenium/SpecFlowSeleniumFeatures.feature
@@ -0,0 +1,14 @@
+﻿Feature: As a Google consumer, I want to learn to trust Google results, 
+		 because I get what I need without clicking through URL results.
+
+Scenario Outline: Spot check quick results
+	When I use Google
+	Then I should see "<result>" when I search for "<query>"
+
+	Examples: 
+	| query                                 | result                          |
+	| Abraham Lincoln assisination date     | April 14, 1865                  |
+	| first man on the moon                 | Neil Armstrong                  |
+	| first man on the moon                 | Apollo 11                       |
+	| pattern altitude lincoln park airport | 1201 MSL                        |
+	| Kakistocracy                          | government by the worst persons |
---
+++ SpecFlowSelenium/SpecFlowSeleniumStepDefs.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.PhantomJS;
+using SpecFlowSelenium;
+using TechTalk.SpecFlow;
+using System;
+using Xunit;
+
+namespace SpecFlowSelenium
+{
+    [Binding]
+    public class SpecFlowSeleniumStepDefs : IDisposable
+    {
+        private string baseURL = "http://www.google.com";
+        private RemoteWebDriver driver;
+
+        public SpecFlowSeleniumStepDefs()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            driver.Navigate().GoToUrl(this.baseURL);
+            driver.FindElementById("lst-ib").Clear();
+        }
+
+        [When(@"I use Google")]
+        public void WhenIUseGoogle()
+        {
+        }
+
+        [Then(@"I should see ""(.*)"" when I search for ""(.*)""")]
+        public void IShouldSeeResultWhenISearchForQuery(string searchResults, string searchTerms)
+        {
+            driver.FindElementById("lst-ib").SendKeys(searchTerms + Keys.Enter);
+            Assert.True(driver.PageSource.Contains(searchResults));
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+  
+    }
+}
